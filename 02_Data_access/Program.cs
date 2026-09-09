ac6dce7e2add01ea78bffc3cbdf24db26b944bab
dashaWeb <57445558+dashaWeb@users.noreply.github.com>
2026-09-09 20:52:56 +0300

    crud

---
+++ 02_Data_access/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Data_access
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string conn = ConfigurationManager.ConnectionStrings["connStr"].ConnectionString;
+            SportShopDb db = new SportShopDb(conn);
+            //db.Create("dumbbells","equipment", 100, 50, "China", 150);
+
+            /*db.Create(new Product()
+            {
+                Name = "Rackets",
+                Type = "Equipment",
+                Quantity = 50,
+                CostPrice = 100,
+                Producer = "China",
+                Price = 250
+            });*/
+
+            List<Product> products = db.GetAll();
+            foreach (var item in products)
+            {
+                Console.WriteLine($"{item.Id,-15}{item.Name, -20}{item.Price, 10}");
+            }
+
+            Product pr = db.GetOneProduct(1);
+            if(pr == null)
+                Console.WriteLine("Product not found");
+            else
+            {
+                Console.WriteLine($"{pr.Id,-10}{pr.Name,-20} {pr.Quantity,-10}");
+                pr.Quantity = 25;
+                db.Update(pr);
+
+            }
+
+            db.Delete(1011);
+            Console.WriteLine();
+            foreach (var item in db.GetAll())
+            {
+                Console.WriteLine($"{item.Id,-15}{item.Name,-20}{item.Price,10}");
+            }
+        }
+    }
+}
---
+++ 02_Data_access/SportShopDb.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Data_access
+{
+    /*
+       [C] - Create 
+       [R] - Read 
+       [U] - Update
+       [D] - Delete
+    */
+    class SportShopDb
+    {
+        private SqlConnection connection;
+        private string connectionString;
+
+        public SportShopDb(string connectionString)
+        {
+            this.connectionString = connectionString;
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+            Console.WriteLine("Database is ready to work!");
+        }
+
+        public void Create(Product product)
+        {
+            string cmdText = $@"insert into Products
+                                values(@name, @type, @quantity,
+                                        @costPrice, @producer, @price)";
+            SqlCommand cmd = new SqlCommand(cmdText, connection);
+
+            SqlParameter parameter = new SqlParameter()
+            {
+                ParameterName = "name",
+                SqlDbType = System.Data.SqlDbType.NVarChar,
+                Value = product.Name
+            };
+            cmd.Parameters.Add(parameter);
+
+            cmd.Parameters.AddWithValue("type", product.Type);
+            cmd.Parameters.AddWithValue("quantity", product.Quantity);
+            cmd.Parameters.AddWithValue("costPrice", product.CostPrice);
+            cmd.Parameters.AddWithValue("producer", product.Producer);
+            cmd.Parameters.AddWithValue("price", product.Price);
+
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Product was added to database");
+
+        }
+        private List<Product> GetProductsByQuery(SqlDataReader reader)
+        {
+            List<Product> products = new List<Product>();
+            while (reader.Read())
+            {
+                products.Add(new Product()
+                {
+                    Id = (int)reader[0],
+                    Name = (string)reader[1],
+                    Type = (string)reader[2],
+                    Quantity = (int)reader[3],
+                    CostPrice = (int)reader[4],
+                    Producer = (string)reader[5],
+                    Price = (int)reader[6]
+                });
+            }
+            reader.Close();
+            return products;
+        }
+        public List<Product> GetAll()
+        {
+            string cmdText = @"select * from Products";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+
+            var reader = command.ExecuteReader();
+            return this.GetProductsByQuery(reader);
+           
+        }
+        public Product GetOneProduct(int id)
+        {
+            string cmdText = $@"select * from Products where Id = {id}";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            return this.GetProductsByQuery(reader).FirstOrDefault();
+        }
+        public void Update(Product product)
+        {
+            string cmdText = $@" update Products 
+                                set Name = @name,
+                                TypeProduct = @type,
+                                Quantity = @quantity,
+                                CostPrice = @costPrice,
+                                Producer = @producer,
+                                Price = @price
+                                where Id = {product.Id}";
+            SqlCommand cmd = new SqlCommand(cmdText, connection);
+            cmd.Parameters.AddWithValue("name", product.Name);
+            cmd.Parameters.AddWithValue("type", product.Type);
+            cmd.Parameters.AddWithValue("quantity", product.Quantity);
+            cmd.Parameters.AddWithValue("costPrice", product.CostPrice);
+            cmd.Parameters.AddWithValue("producer", product.Producer);
+            cmd.Parameters.AddWithValue("price", product.Price);
+
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Product was update to database");
+        }
+        public void Delete(int id)
+        {
+            string cmdText = $@"delete Products where Id = {id}";
+            SqlCommand command = new SqlCommand(cmdText, connection);
+            command.ExecuteNonQuery();
+            Console.WriteLine("Product was deleted from database");
+        }
+
+    }
+}
